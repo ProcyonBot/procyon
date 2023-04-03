1ba5a0be74efd91a6808134e0c784cc9f111c5cf
letters <46039037+keli5@users.noreply.github.com>
2023-04-02 21:50:41 -0500

    Update ProcyonExceptions.kt

---
+++ src/main/kotlin/bot/procyon/util/ProcyonExceptions.kt
@@ -0,0 +1,12 @@
+package bot.procyon.util
+
+open class ProcyonCommandException(cmdName: String? = null, message: String? = null, cause: Throwable? = null) : Exception(message, cause)
+
+class ProcyonNeedsArgsException(cmdName: String? = null, message: String? = "Command $cmdName requires arguments", cause: Throwable? = null)
+    : ProcyonCommandException(cmdName, message, cause)
+
+class ProcyonDisabledException(cmdName: String? = null, message: String? = "Command $cmdName is disabled", cause: Throwable? = null)
+    : ProcyonCommandException(cmdName, message, cause)
+
+class ProcyonChecksException(cmdName: String? = null, message: String? = "Command $cmdName failed checks", cause: Throwable? = null)
+    : ProcyonCommandException(cmdName, message, cause)
