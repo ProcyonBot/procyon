a9b8de8fc5f1ffa30dd58f66a0e5b3dd8bc36d5f
letters <46039037+keli5@users.noreply.github.com>
2023-04-05 21:02:31 -0500

    Add getUserOrNull

--- src/main/kotlin/bot/procyon/util/UserUtil.kt
+++ src/main/kotlin/bot/procyon/util/UserUtil.kt
@@ -1,7 +1,30 @@
 package bot.procyon.util
 
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.Kord
+import dev.kord.core.behavior.UserBehavior
 import dev.kord.core.entity.Member
 import dev.kord.core.entity.User
 
 fun Member.displayAvatar() = memberAvatar ?: avatar ?: defaultAvatar
 fun User.displayAvatar() = avatar ?: defaultAvatar
+
+suspend fun getUserOrNull(string: String?, kord: Kord): User? {
+    if (string.isNullOrBlank()) return null
+
+    var parsedId: Snowflake = Snowflake(0)
+
+    try {
+        parsedId = Snowflake(string)
+    } catch (e: Exception) {
+        if (string.startsWith("<@")) {
+            parsedId = Snowflake(
+                string.replace("<", "")
+                    .replace(">", "")
+                    .replace("@", "")
+                    .replace("!", "")
+            )
+        }
+    }
+    return UserBehavior(parsedId, kord).asUserOrNull()
+}
