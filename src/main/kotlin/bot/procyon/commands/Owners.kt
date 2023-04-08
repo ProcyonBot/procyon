895b557ef2003c9c448cbc2cf692cc1a5fa27ac5
letters <46039037+keli5@users.noreply.github.com>
2023-04-07 22:06:55 -0500

    Owners command

---
+++ src/main/kotlin/bot/procyon/commands/Owners.kt
@@ -0,0 +1,31 @@
+package bot.procyon.commands
+
+import bot.procyon.util.EmbedColor
+import bot.procyon.util.getUserOrNull
+import dev.kord.core.behavior.reply
+import dev.kord.core.entity.Message
+import dev.kord.rest.builder.message.create.embed
+
+class Owners : Command() {
+    override val name = "owners"
+
+    override suspend fun execute(message: Message, args: List<String?>) {
+
+        message.reply {
+            embed {
+                title = "Bot owners"
+                color = EmbedColor.DEFAULT.value
+
+                config.superusers.forEach {
+                    val user = getUserOrNull(it.toString(), kord)!!
+                    field {
+                        name = user.tag
+                        value = user.id.toString()
+                        inline = true
+                    }
+                }
+            }
+        }
+
+    }
+}
