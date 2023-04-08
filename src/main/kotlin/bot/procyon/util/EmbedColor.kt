54a184c8fc4be33df313418a3d6a24817a88532a
letters <46039037+keli5@users.noreply.github.com>
2023-04-07 22:06:09 -0500

    New neutral embed color

--- src/main/kotlin/bot/procyon/util/EmbedColor.kt
+++ src/main/kotlin/bot/procyon/util/EmbedColor.kt
@@ -5,5 +5,6 @@ import dev.kord.common.Color
 enum class EmbedColor(val value: Color) {
     SUCCESS(Color(48, 192, 48)),
     WARN(Color(226, 226, 12)),
-    ERROR(Color(192, 48, 48))
+    ERROR(Color(192, 48, 48)),
+    DEFAULT(Color(160, 160, 160))
 }
