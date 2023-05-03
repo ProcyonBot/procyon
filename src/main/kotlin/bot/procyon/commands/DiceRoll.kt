b63ff5fcd7892bc57736f942c104ff2342ee632a
letters <46039037+keli5@users.noreply.github.com>
2023-05-02 20:09:41 -0500

    DiceRoll command

---
+++ src/main/kotlin/bot/procyon/commands/DiceRoll.kt
@@ -0,0 +1,37 @@
+package bot.procyon.commands
+
+import bot.procyon.util.getResource
+import dev.kord.core.behavior.reply
+import dev.kord.core.entity.Message
+import dev.kord.rest.builder.message.create.embed
+import io.ktor.client.request.forms.*
+import io.ktor.utils.io.jvm.javaio.*
+
+fun diceAliases(sides: Int): List<String> = listOf(sides.toString(), "d${sides.toString()}")
+
+class DiceRoll : Command() {
+    override val name = "diceroll"
+    override val aliases = listOf("roll", "dice")
+
+    override suspend fun execute(message: Message, args: List<String?>) {
+        val type = when (args.firstOrNull()) {
+                in diceAliases(6) -> "d6" //TODO: Other types of dice!
+                in diceAliases(8) -> "d8"
+                //in diceAliases(10) -> "d10"
+                //in diceAliases(12) -> "d12"
+                //in diceAliases(20) -> "d20"
+                else -> "d6"
+            }
+        val max = type.drop(1).toInt()
+        val roll = (1..max).random()
+
+        message.reply {
+            files += addFile("dice.png", ChannelProvider { getResource("/dice/$type/${roll}.png")!!.toByteReadChannel() })
+            embed {
+                title = "Rolled a $type and got $roll!"
+                image = "attachment://dice.png"
+            }
+
+        }
+    }
+}
--- src/main/kotlin/bot/procyon/util/KordUtil.kt
+++ src/main/kotlin/bot/procyon/util/KordUtil.kt
@@ -3,8 +3,12 @@ package bot.procyon.util
 import dev.kord.core.behavior.reply
 import dev.kord.core.entity.Message
 import dev.kord.rest.builder.message.EmbedBuilder
+import java.io.InputStream
 
 suspend fun Message.reply(content: String) = reply { this.content = content }
 suspend fun Message.reply(embed: EmbedBuilder) = reply { embeds.add(embed) }
 
 inline fun embed(block: EmbedBuilder.() -> Unit) = EmbedBuilder().also(block)
+
+fun getResource(path: String): InputStream? =
+    object {}.javaClass.getResourceAsStream(path)
