fcbf8b1fbd00d6461ce857076e426e0499b5e241
letters <46039037+keli5@users.noreply.github.com>
2023-06-18 12:53:35 -0500

    utility functions for database

---
+++ src/main/kotlin/bot/procyon/util/DatabaseUtil.kt
@@ -0,0 +1,25 @@
+package bot.procyon.util
+
+import bot.procyon.models.User
+import bot.procyon.models.guild
+import bot.procyon.models.user
+import dev.kord.common.entity.Snowflake
+import org.komapper.core.dsl.Meta
+import org.komapper.core.dsl.QueryDsl
+import org.komapper.core.dsl.query.Query
+import org.komapper.core.dsl.query.singleOrNull
+import org.komapper.r2dbc.R2dbcDatabase
+
+val user =  Meta.user
+val guild = Meta.guild
+
+suspend fun R2dbcDatabase.getOrCreateUser(id: Snowflake): User {
+    val selectQuery: Query<User?> = QueryDsl.from(user).where { user.id eq id }.singleOrNull()
+
+    return runQuery { selectQuery }
+        ?: this.runQuery {
+            QueryDsl.insert(user).single(
+                User(id, 0, 0f)
+            ).returning()
+        }
+}
