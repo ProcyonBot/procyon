30ae4963d7331d18e0dac2d8b2a322545a29f5d3
letters <46039037+keli5@users.noreply.github.com>
2023-06-18 08:24:18 -0500

    user is a reserved word?

--- src/main/kotlin/bot/procyon/models/Guild.kt
+++ src/main/kotlin/bot/procyon/models/Guild.kt
@@ -2,9 +2,10 @@ package bot.procyon.models
 
 import org.komapper.annotation.KomapperEntity
 import org.komapper.annotation.KomapperId
+import org.komapper.annotation.KomapperOneToMany
 
 @KomapperEntity
-//@KomapperOneToMany(targetEntity = User::class)
+@KomapperOneToMany(targetEntity = User::class)
 data class Guild(
     @KomapperId
     val id:     Long
